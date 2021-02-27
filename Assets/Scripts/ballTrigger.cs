8d332ab52c719ba2c1f6e867f3f34cb2ab815359
58485607+cvmaxie@users.noreply.github.com <58485607+cvmaxie@users.noreply.github.com>
2021-02-27 15:34:41 -0500

    work in progress scripting camera movement

---
+++ Assets/Scripts/ballTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballTrigger : MonoBehaviour {
+    SpriteRenderer ballRenderer;
+    public int currentTrigger;
+
+    void Start() {
+        currentTrigger = 0;
+    }
+
+    void FixedUpdate(){}
+
+    void OnTriggerEnter2D(Collider2D other) { //checking collision with cam triggers
+        if (other.gameObject.name == "triggerPos1") {
+            currentTrigger = 1;
+        } else if (other.gameObject.name == "triggerPos2") {
+            currentTrigger = 2;
+        } else if (other.gameObject.name == "triggerPos3") {
+            currentTrigger = 3;
+        }
+    }
+}
---
+++ Assets/Scripts/camSwitch.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camSwitch : MonoBehaviour {
+    public Transform followTransform;
+    public GameObject thisBall;
+
+    private ballTrigger thisBallTrigger;
+
+    public Vector3 pos1, pos2, pos3, targetPos; //3 camera positions to switch to, current intended position
+
+    Camera mainCam;
+    Vector3 smoothPos;
+    public float smoothRate;
+
+    void Start() {
+        mainCam = gameObject.GetComponent<Camera>();
+        targetPos = pos1;
+        thisBallTrigger = thisBall.GetComponent<ballTrigger>();
+    }
+
+    void FixedUpdate() {
+        smoothCam();
+        targetSwitcher();
+    }
+
+    void smoothCam() { //for smoothing & moving camera
+        smoothPos = Vector3.Lerp( gameObject.transform.position, targetPos, smoothRate); //lerp between intended location and current location
+        gameObject.transform.position = smoothPos;
+    }
+
+    void targetSwitcher() {
+        if (thisBallTrigger.currentTrigger == 1) {
+            targetPos = pos2;
+        }
+    }
+}
+
